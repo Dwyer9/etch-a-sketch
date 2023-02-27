bc1922c4469066c8ca37f0292475d82d92fdab20
Mathew Dwyer <120291154+Dwyer9@users.noreply.github.com>
2023-02-27 19:08:10 +1100

    Added random colour option

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const container = document.querySelector('.container');
 const input = document.querySelector('.number');
 const resetBtn = document.querySelector('.resetBtn');
+const randomBtn = document.querySelector('.randomBtn');
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 const createSquares = function (num) {
   for (let i = 0; i < num ** 2; i++) {
@@ -14,6 +19,8 @@ const createSquares = function (num) {
   const boxes = document.querySelectorAll('.empty');
   boxes.forEach((box) => {
     box.addEventListener('mouseenter', () => {
+      //Add only draw on Mouse Down
+      // if (mouseDown) box.style.backgroundColor = 'black';
       box.style.backgroundColor = 'black';
     });
   });
@@ -27,5 +34,35 @@ resetBtn.addEventListener('click', () => {
   console.log(number);
   container.innerHTML = '';
   createSquares(number);
+  input.value = '';
+});
+
+const createSquaresRandom = function (num) {
+  for (let i = 0; i < num ** 2; i++) {
+    let newSquare = document.createElement('div');
+    newSquare.classList.add('empty');
+    container.appendChild(newSquare);
+  }
+
+  container.style.gridTemplateColumns = `repeat(${num}, auto)`;
+
+  const boxes = document.querySelectorAll('.empty');
+  boxes.forEach((box) => {
+    const randomRed = Math.floor(Math.random() * 255 + 1);
+    const randomBlue = Math.floor(Math.random() * 255 + 1);
+    const randomGreen = Math.floor(Math.random() * 255 + 1);
+
+    box.addEventListener('mouseenter', () => {
+      box.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+    });
+  });
+};
+
+randomBtn.addEventListener('click', () => {
+  let number = input.value;
+  if (number > 100) number = 100;
+  console.log(number);
+  container.innerHTML = '';
+  createSquaresRandom(number);
   input.value = '';
 });
